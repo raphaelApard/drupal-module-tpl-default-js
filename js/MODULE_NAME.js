28229dfa81912095fe24ef6e1c51aa8c60c1c84e
Raphael Apard <raphael.apard.ext@ovhcloud.com>
2023-06-15 11:57:41 +0200

    Initial commit

---
+++ js/MODULE_NAME.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * test2 behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.MODULE_NAME = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
